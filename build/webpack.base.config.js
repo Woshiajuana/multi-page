05445a2bc9f7172692293d258280c5ef7e890575
Woshiajuana <zhigang.chen@owulia.com>
2021-06-05 21:15:24 +0800

    subcode

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -49,6 +49,26 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                "useBuiltIns": "usage",
+                                "corejs": {
+                                    "version": 3
+                                },
+                                "targets": {
+                                    "chrome": "60",
+                                    "firefox": "60",
+                                    "ie": "9",
+                                    "safari": "10",
+                                    "edge": "17"
+                                }
+                            },
+                        ]
+                    ],
+                }
             },
             // // html
             // {
