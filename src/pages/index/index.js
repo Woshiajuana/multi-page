6f5177570f56822cc64069e2ed1a0a3fff0d4cf7
Woshiajuana <zhigang.chen@owulia.com>
2021-06-09 16:34:25 +0800

    subcode

---
+++ src/pages/index/index.js
@@ -0,0 +1,6 @@
+
+import './index.scss'
+
+console.log(1);
+console.log(2);
+console.log(3);
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,2 @@
 
 import './index.scss'
-
-const title = document.getElementById('title');
-title.style = `display: flex`;
