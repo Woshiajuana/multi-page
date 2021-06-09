6638a2c9e4cfcbede85fb258d3c057f434a8ddeb
Woshiajuana <zhigang.chen@owulia.com>
2021-06-09 17:53:30 +0800

    subcode

---
+++ src/components/react/NavBar/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+import './index.scss'
+
+export default function NavBar() {
+    return (
+        <div className="nav-bar">
+            头部菜单
+        </div>
+    );
+};
