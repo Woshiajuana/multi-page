123f10e8e9c43dec67f7616ee3d09b1e5fed4dc8
Woshiajuana <zhigang.chen@owulia.com>
2021-06-09 17:33:56 +0800

    subcode

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -226,7 +226,7 @@ if (isDevServer) {
     ];
     if (useCompress) {
         plugins.push(new CompressionPlugin({
-            filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+            // filename: '[name].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
             algorithm: 'gzip', // 算法
             test: new RegExp('\\.(js|css)$'), // 压缩 js 与 css
             threshold: 10240, // 只处理比这个值大的资源。按字节计算
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,60 @@
 
-import './index.scss'
-import 'antd/dist/antd.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Button, DatePicker, version } from 'antd'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import 'antd/dist/antd.min.css'
 
-ReactDOM.render(
-    <div className="App">
-        <h1>antd version: {version}</h1>
-        <DatePicker />
-        <Button type="primary" style={{ marginLeft: 8 }}>
-            Primary Button
-        </Button>
-    </div>,
-    document.getElementById("root")
-);
+import './index.scss'
+
+const Login = () => {
+    const onFinish = (values) => {
+        console.log(values);
+        window.location.href = './index.html';
+    };
+    return (
+        <div className="login-wrap">
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your Username!' }]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your Password!' }]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+
+ReactDOM.render(<Login/>, document.getElementById("root"));
